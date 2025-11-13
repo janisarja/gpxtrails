89b36350df4bf73f944844ee8a049286aabed691
Jani Sarja <jani.sarja@tuni.fi>
2025-11-13 02:36:45 +0200

    Add simple trail drawing/editing functionality

--- src/app/trails/new/page.jsx
+++ src/app/trails/new/page.jsx
@@ -2,7 +2,7 @@ const Page = () => {
   return (
     <div>
       <h1>Create a New Trail</h1>
-      <a href='./editor'><button>New Trail</button></a>
+      <a href='./new/editor'><button>New Trail</button></a>
     </div>
   );
 }
--- src/components/map-editor.jsx
+++ src/components/map-editor.jsx
@@ -1,25 +1,69 @@
 'use client';
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { MapContainer, TileLayer, useMap } from 'react-leaflet';
 import 'leaflet-editable';
+import 'leaflet-easybutton'
 import 'leaflet/dist/leaflet.css';
+import { createElement, Route, Eraser } from 'lucide';
 
+const drawIcon = createElement(Route).outerHTML;
+const eraseIcon = createElement(Eraser).outerHTML;
 
 const EditableTrailLayer = () => {
   const map = useMap();
+  //const [eraseMode, setEraseMode] = useState(false);
 
   useEffect(() => {
-    console.log('Here');
+    console.log(map._eraseMode);
     if (!map || !map.editTools) return;
-    console.log('Map Editor - map and editTools available');
 
     // Create an editable polyline
-    const polyline = L.polyline([], { color: 'green' }).addTo(map);
+    const polyline = L.polyline([], { color: 'blue' }).addTo(map);
     polyline.enableEdit(map);
+    polyline.editor.continueForward();
 
-    // Start drawing immediately
-    map.editTools.startPolyline();
+    // Button to go between draw and erase modes
+    var editModeButton = L.easyButton({
+      states: [
+        {
+          stateName: 'draw-mode',
+          icon: drawIcon,
+          title: 'Switch to Erase Mode',
+          onClick: (btn, map) => {
+            map._eraseMode = true;
+            btn.state('erase-mode');
+          }
+        },
+        {
+          stateName: 'erase-mode',
+          icon: eraseIcon,
+          title: 'Switch to Draw Mode',
+          onClick: (btn, map) => {
+            map._eraseMode = false;
+            btn.state('draw-mode');
+          }
+        }
+      ]
+    }).addTo(map);
+
+    const clickHandler = (e) => {
+      console.log(map._eraseMode);
+      if (map._eraseMode) {
+        console.log('Erasing vertex');
+      } else {
+        e.cancel();
+        map.editTools.drawing() ? map.editTools.stopDrawing() : e.vertex.continue();
+      }
+    }
+
+    map.on('editable:vertex:click', clickHandler);
+
+    return () => {
+      map.off('editable:vertex:click', clickHandler);
+      map.removeControl(editModeButton);
+      map.removeLayer(polyline);
+    }
   }, [map]);
 
   return null;
@@ -27,19 +71,23 @@ const EditableTrailLayer = () => {
 
 const MapEditor = () => {
   return (
-    <MapContainer 
-      editable={true}
-      center={[51.505, -0.09]}
-      zoom={13} 
-      scrollWheelZoom={true}
-      style={{ height: '400px', width: '100%' }}
-    >
-      <TileLayer
-        attribution='Map Data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, SRTM | Map Display: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href=https://creativecommons.org/licenses/by-sa/3.0/>CC-BY-SA<a>)'
-        url='https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png'
-      />
-      <EditableTrailLayer />
-    </MapContainer>
+    <div>
+      <p> In draw mode click on one of the ends to start drawing. Click on any point to stop drawing. Click and drag points to move them. Add more points in the middle of the line by clicking and dragging on them. In erase mode click on points to delete them. Use button in top left corner to switch between draw mode and erase mode.</p>
+      <MapContainer 
+        editable={true}
+        center={[51.505, -0.09]}
+        zoom={13} 
+        zoomControl={false}
+        scrollWheelZoom={true}
+        style={{ height: '400px', width: '100%' }}
+      >
+        <TileLayer
+          attribution='Map Data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, SRTM | Map Display: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href=https://creativecommons.org/licenses/by-sa/3.0/>CC-BY-SA<a>)'
+          url='https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png'
+        />
+        <EditableTrailLayer />
+      </MapContainer>
+    </div>
   );
 }
 
