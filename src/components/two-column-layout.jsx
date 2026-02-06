726276bf5a163d7a3702a9665b3bdabca814cd07
Jani Sarja <jani.sarja@tuni.fi>
2026-02-06 22:45:09 +0200

    Fix horizontal text overflow

--- src/components/two-column-layout.jsx
+++ src/components/two-column-layout.jsx
@@ -3,7 +3,7 @@
 export default function TwoColumnLayout({ left, right }) {
   return (
     <div className="flex bg-white">
-      <div className="w-2/5 overflow-auto p-6 max-h-[calc(100vh-var(--header-height))]">{left}</div>
+      <div className="w-2/5 min-w-0 overflow-auto p-6 max-h-[calc(100vh-var(--header-height))] break-words">{left}</div>
       <div className="w-3/5 h-[calc(100vh-var(--header-height))]">
         <div className="h-full">{right}</div>
       </div>
