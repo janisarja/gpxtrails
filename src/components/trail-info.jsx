776233425a0a525d935b36a3253a26ad060eedc2
Jani Sarja <jani.sarja@tuni.fi>
2026-02-06 22:45:09 +0200

    Fix horizontal text overflow

--- src/components/trail-info.jsx
+++ src/components/trail-info.jsx
@@ -2,8 +2,12 @@ const TrailInfo = async ({ trail }) => {
   return (
     <>
       <h1 className="text-2xl font-semibold text-gray-900">{trail.name}</h1>
-      <p className="text-sm text-gray-600">Kuvaus: {trail.description}</p>
-      <p className="text-sm text-gray-600">Pituus: {trail.distance_km} km</p>
+      <p className="text-sm text-gray-600">
+        <b>Description:</b> {trail.description}
+      </p>
+      <p className="text-sm text-gray-600">
+        <b>Distance:</b> {trail.distance_km} km
+      </p>
     </>
   );
 }
