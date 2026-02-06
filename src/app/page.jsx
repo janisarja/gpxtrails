f1ec84632909a98afe5840838606c32d923f4ce0
Jani Sarja <jani.sarja@tuni.fi>
2026-02-06 23:13:13 +0200

    Add home page content, make buttons client components

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,31 @@
+import Link from "next/link";
+import ButtonLarge from "../components/button-large";
+
+const { default: Card } = require("../components/card")
+
 const Page = () => {
   return (
-    <h1>Home</h1>
+    <div>
+      <p className=" p-4 m-10 max-w-md mx-auto text-center">
+        A platform for discovering, creating and sharing trails for trail running and hiking.
+      </p>
+      <Card 
+        title="Discover"
+      >
+        <p>
+          Find trails from other outdoor enthusiasts!
+        </p>
+        <Link href="/trails"><ButtonLarge text="Browse Trails"/></Link>
+      </Card>
+      <Card 
+        title="Create"
+      >
+        <p>
+          Create and share your own trails with the trail editor!
+        </p>
+        <Link href="/trails/new"><ButtonLarge text="Make Your Own"/></Link>
+      </Card>
+    </div>
   );
 }
 
--- src/components/button-large.jsx
+++ src/components/button-large.jsx
@@ -1,9 +1,11 @@
+'use client';
+
 const ButtonLarge = ({ text, onClick=() => {}, type = "button" }) => {
   return (
     <button 
       type={type}
       onClick={onClick}
-      className="px-3 py-2 my-3 mr-3 bg-gray-900 text-white rounded-sm hover:bg-gray-800 cursor-pointer">
+      className="px-3 py-2 my-3 bg-gray-900 text-white rounded-sm hover:bg-gray-800 cursor-pointer">
       {text}
     </button>
   )
