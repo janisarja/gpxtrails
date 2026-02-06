31fa3b62fcdd73962fb5a6b7914c70d71981b924
Jani Sarja <jani.sarja@tuni.fi>
2026-02-06 23:13:13 +0200

    Add home page content, make buttons client components

--- src/components/button-small.jsx
+++ src/components/button-small.jsx
@@ -3,7 +3,7 @@ const ButtonSmall = ({ text, onClick=() => {}, type = "button" }) => {
     <button 
       type={type}
       onClick={onClick}
-      className="px-2 py-1 my-3 mr-3 bg-gray-100 rounded-sm border text-sm hover:bg-gray-200 hover:border-gray-400 transition-colors cursor-pointer">
+      className="px-2 py-1 my-3 bg-gray-100 rounded-sm border text-sm hover:bg-gray-200 hover:border-gray-400 transition-colors cursor-pointer">
       {text}
     </button>
   )
