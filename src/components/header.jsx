9f7a0b68b22ec4f006362bcc31fe177d46dd6211
Jani Sarja <jani.sarja@tuni.fi>
2026-02-07 00:25:41 +0200

    Resolve conflicts

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -7,15 +7,7 @@ const Header = () => {
     <header className="border-b bg-white" style={{ height: 'var(--header-height)' }}>
       <div className="container h-full">
         <div className="flex items-center justify-between h-full">
-<<<<<<< HEAD
-<<<<<<< HEAD
           <h1 className="text-lg font-semibold text-slate-900 m-3">GPX Trails</h1>
-=======
-          <h1 className="text-lg font-semibold text-slate-900">GPX Trails</h1>
->>>>>>> 119d19f (Add a two column layout for pages with maps)
-=======
-          <h1 className="text-lg font-semibold text-slate-900 m-3">GPX Trails</h1>
->>>>>>> b460069 (Add styling and refactor buttons)
           <Nav />
         </div>
       </div>
